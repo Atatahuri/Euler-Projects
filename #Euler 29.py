b06c118e0df7a0c78da543246ff9d7bb4d43da3a
Atatahuri <52317261+Atatahuri@users.noreply.github.com>
2025-03-22 06:03:54 +0000

    Euler 32

--- #Euler 29.py
+++ #Euler 29.py
@@ -1,8 +1,14 @@
-#Euler 29 
-distinct_powers = [] 
-for a in range (2,101):
-    for b in range(2,101): 
-        if a ** b not in distinct_powers: 
-            distinct_powers.append(a ** b)
+# #Euler 29 
+# distinct_powers = [] 
+# for a in range (2,101):
+#     for b in range(2,101): 
+#         if a ** b not in distinct_powers: 
+#             distinct_powers.append(a ** b)
 
-print(len(distinct_powers))
+# # print(len(distinct_powers)
+
+i = 0
+while i < 1000000000000:
+     i += 1
+
+print("Largest_Order")
